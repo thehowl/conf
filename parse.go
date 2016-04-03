6b8467e0436d45cc1a6321993a5ddd3858e29e99
Howl <git@howl.moe>
2016-04-03 12:20:52 +0200

    Initial commit

---
+++ parse.go
@@ -0,0 +1,80 @@
+package conf
+
+// FieldValue is a field=value pair in the configuration.
+type FieldValue struct {
+	Field string
+	Value string
+}
+
+const (
+	currentName = iota
+	currentValue
+	currentComment
+)
+
+// Parse converts some bytes into various FieldValue pairs.
+func Parse(data []byte) []FieldValue {
+	fvs := []FieldValue{}
+	var (
+		fieldName  string
+		fieldValue string
+		nextNormal bool
+		current    byte
+	)
+
+	for _, c := range data {
+		if current == currentComment && c != '\n' {
+			continue
+		}
+		if nextNormal {
+			switch current {
+			case currentName:
+				fieldName += string(c)
+			case currentValue:
+				fieldValue += string(c)
+			}
+			nextNormal = false
+			continue
+		}
+		switch c {
+		case '=':
+			switch current {
+			// if we are still at the name, let's switch to a value.
+			case currentName:
+				current = currentValue
+			// if we are already at the value, treat the = character like any other sign
+			case currentValue:
+				fieldValue += string(c)
+			}
+		case ';':
+			current = currentComment
+		case '\n':
+			if fieldName != "" && fieldValue != "" {
+				fvs = append(fvs, FieldValue{
+					Field: fieldName,
+					Value: fieldValue,
+				})
+			}
+			fieldName = ""
+			fieldValue = ""
+			current = currentName
+		case '\\':
+			nextNormal = true
+		default:
+			switch current {
+			case currentName:
+				fieldName += string(c)
+			case currentValue:
+				fieldValue += string(c)
+			}
+			nextNormal = false
+		}
+	}
+	if fieldName != "" && fieldValue != "" {
+		fvs = append(fvs, FieldValue{
+			Field: fieldName,
+			Value: fieldValue,
+		})
+	}
+	return fvs
+}
